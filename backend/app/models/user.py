b70dbd30e55bcdd1374fcdad257cba287485324b
omar zroud <omarzroud23@gmail.com>
2026-03-04 03:48:37 +0100

    SQLAlchemy auth update

---
+++ backend/app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey, Integer
+from app.models.base import Base
+from sqlalchemy import Enum as SAEnum
+from sqlalchemy.sql import func
+import enum
+
+class UserRole(enum.Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id           = Column(String(50), primary_key=True)
+    email        = Column(String(255), unique=True, nullable=False)
+    password_hash= Column(String(255), nullable=False)
+    role         = Column(SAEnum(UserRole), default=UserRole.USER)
+    is_active    = Column(Boolean, default=True, nullable=False)
+    is_verified  = Column(Boolean, default=False, nullable=False)
+    created_at   = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    last_login   = Column(DateTime, timezone=True, nullable=True)
